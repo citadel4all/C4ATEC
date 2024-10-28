db6af21fc01a3adaa0848148af9051950070c623
Louis Obuseh <73742256+citadel4all@users.noreply.github.com>
2024-10-28 10:59:22 +0100

    Update getmovie.js

--- getmovie.js
+++ getmovie.js
@@ -4,7 +4,7 @@ const IMGPATH = "https://image.tmdb.org/t/p/w1280";
 const SEARCHAPI = `https://api.themoviedb.org/3/search/movie?&api_key=${APIKEY}&query=`;
 
 const main = document.createElement("div");
-main.id = "main";
+main.id = "main-page";
 document.body.appendChild(main);
 
 const notification = document.createElement("div");
