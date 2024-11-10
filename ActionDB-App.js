df9a7f67ecfb490f5a1d8b1b1b13798336cccc52
Louis Obuseh <73742256+citadel4all@users.noreply.github.com>
2024-11-10 13:10:27 +0100

    UrlByPlatformID ActionDB-App.js

--- ActionDB-App.js
+++ ActionDB-App.js
@@ -1,5 +1,5 @@
 const APIKEY = '04c35731a5ee918f014970082a0088b1';
-const APIURL = `https://api.themoviedb.org/3/discover/movie?api_key=${APIKEY}&with_genres=28&certification_country=US&certification.gte=PG&include_adult=false`;
+const APIURL = `https://api.themoviedb.org/3/discover/movie?api_key=${APIKEY}&with_genres=28&certification_country=US&certification.lte=PG&include_adult=false`;
 const IMGPATH = "https://image.tmdb.org/t/p/w1280";
 const DEFAULT_IMG = "https://via.placeholder.com/500x750?text=No+Image+Available";
 
@@ -86,7 +86,7 @@ function urlByPlatform(platform) {
   }
 
   // Construct the TMDb URL
-  const url = `${baseUrl}?api_key=${apiKey}&language=${language}&sort_by=popularity.desc&include_adult=${adult}&with_genres=${genre}&with_watch_providers=${watchProvider}&watch_region=US`;
+  const url = `${baseUrl}?api_key=${apiKey}&language=${language}&sort_by=popularity.desc&include_adult=${adult}&with_genres=${genre}&with_watch_providers=${platformID}&watch_region=US`;
 
   return url;
 }
